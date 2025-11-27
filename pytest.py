c3b7a88788ca292c097460812f71f8f63f76cd7f
Pabloz21v <pabloz21v@hotmail.com>
2025-11-27 19:35:02 -0300

    sara safe

--- pytest.py
+++ pytest.py
@@ -1,11 +1,40 @@
-productos = ["laptop", "mouse", "teclado", "monitor"]
-precios = [899.99, 25.50, 75.00, 199.99]
+menu = """
+Elegí una opción:
+1) Cifrar un mensaje
+2) Decifrar un mensaje
+"""
+print(menu) #Le mostramos al usuario las opciones a elegir 
 
-productos_info = [f"{producto.title()}: ${precio}" for producto, precio in zip(productos, precios)]
-productos_descuento = [f"{producto.title()} (con descuento)" for producto, precio in zip(productos, precios) if precio > 100]
+opcion = int(input("Ingresá el número de la opción: ")) # Pedimos una opción al usuario
 
-print("Información de productos:")
-for info in productos_info:
-    print(f"- {info}")
+mensaje = input("Ingresá el mensaje: ") # Pedimos el mensaje a cifrar/decifrar
 
-print(f"\nProductos con descuento: {productos_descuento}")
+# pedimos el desplazamiento para el cifrado(es la cantidad de caracteres que se mueve hacia atras o adelante)
+desplazamiento = int(input("Ingresá el desplazamiento (Debe ser el mismo numero del 1-10 para cifrar y decifrar): "))
+
+resultado = ""  # variable donde guardamos el resultado
+
+# el bucle for recorrer el mensaje caracter por caracter
+for caracter in mensaje:
+    
+    if caracter:
+        # Transformamos a número (código ASCII)
+        codigo = ord(caracter)# ord convierte una letra a su numero de codigo ASCII
+
+        if opcion == 1:  # Cifrar
+            codigo = codigo + desplazamiento 
+        else:            # Decifrar
+            codigo = codigo - desplazamiento
+
+        # Convertimos de nuevo a caracter
+        nuevo_caracter = chr(codigo) # chr es la convercion contraria de ord (de numero a letra)
+        resultado += nuevo_caracter # el (+=) hace la suma de los caracteres convertidos
+
+    else:
+        # Si no es letra, lo dejamos igual
+        resultado += caracter
+
+# resultado final
+print("Resultado:", resultado)
+
+
