a1b5bdca5dba98e8f270919408b8b3127f9886bc
Pabloz21v <pabloz21v@hotmail.com>
2025-12-01 07:59:43 -0300

    h1 nota final

--- pytest.py
+++ pytest.py
@@ -1,40 +1,15 @@
-menu = """
-Elegí una opción:
-1) Cifrar un mensaje
-2) Decifrar un mensaje
-"""
-print(menu) #Le mostramos al usuario las opciones a elegir 
-
-opcion = int(input("Ingresá el número de la opción: ")) # Pedimos una opción al usuario
-
-mensaje = input("Ingresá el mensaje: ") # Pedimos el mensaje a cifrar/decifrar
-
-# pedimos el desplazamiento para el cifrado(es la cantidad de caracteres que se mueve hacia atras o adelante)
-desplazamiento = int(input("Ingresá el desplazamiento (Debe ser el mismo numero del 1-10 para cifrar y decifrar): "))
-
-resultado = ""  # variable donde guardamos el resultado
-
-# el bucle for recorrer el mensaje caracter por caracter
-for caracter in mensaje:
-    
-    if caracter:
-        # Transformamos a número (código ASCII)
-        codigo = ord(caracter)# ord convierte una letra a su numero de codigo ASCII
-
-        if opcion == 1:  # Cifrar
-            codigo = codigo + desplazamiento 
-        else:            # Decifrar
-            codigo = codigo - desplazamiento
-
-        # Convertimos de nuevo a caracter
-        nuevo_caracter = chr(codigo) # chr es la convercion contraria de ord (de numero a letra)
-        resultado += nuevo_caracter # el (+=) hace la suma de los caracteres convertidos
-
-    else:
-        # Si no es letra, lo dejamos igual
-        resultado += caracter
-
-# resultado final
-print("Resultado:", resultado)
-
+# FINALMENTE imprimimos los resultados del texto cifrado
+# Fue vastante raro y complicado hacerlo, puesto que habia un monton de
+# nuevas cosas que aprendi haciendo esto
+# Lastimosamente no tuvimos mucho tiempo con usted profe, pero fue mejor de
+# lo que esperaba
+# Lo mas probable es que estas cosas nuevas como el ".index"(aunque creo
+# que lo vi antes), "% 26" y ".lower", de haber tenido mas tiempo
+# con usted podria haberlo aprendido sin dudas, aunque toco aprenderlo a la
+# fuerza por Google
+# Surgieron dudas pero nada que el internet no pueda resolver, asi que
+# obvio el tipico "facil" al final de una busqueda no falto,
+# pero esta bueno aprender de esto, ademas que ya la base, lo mas facil lo
+# tenia, la duda era hacer lo complicado,
+# ¿cambiar de lugar la posicion? que-, pero se pudo, asi que ps, esto seria todo. Bye�
 
