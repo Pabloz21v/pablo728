f3806116c3f1a84da3ec2e4283e565d5b4cb99ff
Pabloz21v <pabloz21v@hotmail.com>
2026-04-09 22:36:34 -0300

    s10d1

--- pytest.py
+++ pytest.py
@@ -2,3 +2,13 @@ precio = 99.90
 alimento = "pata de pollo"
 vencimiento = "11/04/06"
 print(alimento, precio, precio, precio, alimento, vencimiento, precio,vencimiento) 
+
+# Esto es un comentario de una línea
+edad = 25  # comentario al final
+
+"""
+Comentario multilínea:
+puedes explicar la lógica completa
+o documentar una función.
+"""
+print(edad)
