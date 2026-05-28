e6f7cda574e1ef2b0b65fae75f1507a2be331d56
Pabloz21v <pabloz21v@hotmail.com>
2026-05-27 22:51:55 -0300

    s12

--- pytest.py
+++ pytest.py
@@ -1,14 +1,3 @@
-pueblo = "Villa Sombra"
-detective = "Elena Ríos"
-asunto = "la campana dorada"
-sospechoso = "el bibliotecario"
-resuelto = True
-
-print(f"""
-*** MISTERIO EN {pueblo} ***
-
-La detective {detective} investigaba el robo de {asunto}.
-Todos señalaban a {sospechoso}, pero {detective} descubrió la verdad.
-¿Se resolvió el caso? {resuelto}
-Así fue, y la paz regresó a {pueblo}.
-""")
+a= "hola"
+b= "chau"
+print(a+b)
