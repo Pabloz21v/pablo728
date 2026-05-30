d484b7a5156f8918c8dcf798abd326e3f897d7b0
Pabloz21v <pabloz21v@hotmail.com>
2026-05-30 14:56:37 -0300

    h1 s11

--- pytest.py
+++ pytest.py
@@ -1,3 +1,7 @@
-a= "B"
-b= "B"
-print(a!=b)
+hay_stock = False
+
+# "not hay_stock" se lee como "si NO hay stock"
+if not hay_stock:
+    print("Lo sentimos, el producto está agotado.")
+else:
+    print("Añadiendo producto al carrito de compras.")
